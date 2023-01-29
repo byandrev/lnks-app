b9ef364df5e7ac750054f0188a04e270e649dd69
byandrev <byandrev@gmail.com>
2023-01-28 19:25:09 -0500

    use hook useUser

--- src/components/MenuAuth.jsx
+++ src/components/MenuAuth.jsx
@@ -1,12 +1,32 @@
 import { Button, ButtonGroup, Flex } from '@chakra-ui/react'
+import useUser from '../hooks/useUser'
+import { login as loginService } from '../services/auth'
 
 function MenuAuth() {
+  const { user, login, logout } = useUser()
+
+  const handleLogin = async () => {
+    const token = await loginService('byandrev@gmail.com', '123')
+    login(token.access_token)
+  }
+
   return (
     <Flex>
-      <ButtonGroup gap={2}>
-        <Button colorScheme="telegram">Sign Up</Button>
-        <Button colorScheme="telegram">Login</Button>
-      </ButtonGroup>
+      {!user ? (
+        <ButtonGroup gap={2}>
+          <Button colorScheme="telegram">Sign Up</Button>
+          <Button colorScheme="telegram" onClick={handleLogin}>
+            Login
+          </Button>
+        </ButtonGroup>
+      ) : (
+        <>
+          <p>Hello {user.username}</p>
+          <Button colorScheme="red" onClick={logout}>
+            Logout
+          </Button>
+        </>
+      )}
     </Flex>
   )
 }
